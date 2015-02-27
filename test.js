6944974e8ceb1b12e1705ee0167bf1d818d10cbc
Marko Raatikka <marko.raatikka@gmail.com>
2015-02-28 00:32:07 +0200

    Add failing test for power of power
    Currently, power of power notation does not work (should be
    computed from right-to-left). E.g. 2^2^3 should amount to 256 and not 64.

--- test.js
+++ test.js
@@ -9,6 +9,9 @@ var equationParser = new EquationParser();
 
 var eq1 = equationParser.parseEquation('2*6+5-2%2-5^3-1+1/5-1');
 var eq2 = equationParser.parseEquation(' 1    + 2+ 4   /2   ');
+var eq3 = equationParser.parseEquation('2^2^3');
 
 assert.equal(eq1.compute(), -109.8, 'should handle computation');
 assert.equal(eq2.compute(), 5, 'should handle whitespaced input');
+// @todo Implement power of power
+assert.equal(eq3.compute(), 256, 'should handle power of power');
