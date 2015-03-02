e76b9a71792981a83dd0d7bc9540fb2062bdb898
Marko Raatikka <marko.raatikka@gmail.com>
2015-03-02 12:05:28 +0200

    Fix jshint

--- EquationParser.js
+++ EquationParser.js
@@ -27,6 +27,58 @@ var SUPPORTED_OPERANDS_REGEX = new RegExp('[\\' + SUPPORTED_OPERANDS.join('\\') 
 function EquationParser() {}
 
 /**
+ * Validates a given equation string by checking that it contains
+ * only numbers (integers or zero-padded floats) and/or supported operands
+ *
+ * @param  {String} equationStr
+ * @throws {Error}  Throws an Error if the input is invalid
+ */
+function validateEquation(equationStr) {
+
+  var regexStr = '[^\\d]{2,}|[^\\d\\.\\' + SUPPORTED_OPERANDS.join('\\') + ']';
+  var validationRegex = new RegExp(regexStr, 'gi');
+
+  if (validationRegex.test(equationStr)) {
+    throw new Error('The equation contains invalid characters. Supported operands: ' + SUPPORTED_OPERANDS.join(', '));
+  }
+
+}
+
+/**
+ * Sanitizes the equation string:
+ *   - strip whitespace
+ *   - pad decimal points with missing zeros
+ *
+ * @param  {String} equationStr The equation
+ * @return {String}             A sanitized equation
+ */
+function sanitizeEquation(equationStr) {
+
+  return equationStr
+    // Strip whitespace
+    .replace(/\s+/g, '')
+    // Pad decimal points with zeros
+    .replace(/(\D)\.(\d)/g, function (m, p1, p2) {
+      return p1 + '0.' + p2;
+    })
+    .replace(/(\d)\.(\D)/g, function (m, p1, p2) {
+      return p1 + '.0' + p2;
+    });
+
+}
+
+/**
+ * Checks if a given string contains operand characters
+ *
+ * @param  {String}  equationStr A string representation of an equation
+ * @return {Boolean}             True if operand characters exist, otherwise false
+ */
+function hasOperands (equationStr) {
+  SUPPORTED_OPERANDS_REGEX.lastIndex = 0;
+  return SUPPORTED_OPERANDS_REGEX.test(equationStr);
+}
+
+/**
  * Parses the string representation of an equation into an Equation instance
  * consisting of integers and/or other Equation instances
  * @param  {String} equationStr A string representation of an equation
@@ -61,58 +113,6 @@ function parseEquation(equationStr, operands) {
 
   return new Equation(values, operand);
 
-}
-
-/**
- * Validates a given equation string by checking that it contains
- * only numbers (integers or zero-padded floats) and/or supported operands
- *
- * @param  {String} equationStr
- * @throws {Error}  Throws an Error if the input is invalid
- */
-function validateEquation(equationStr) {
-
-  var regexStr = '[^\\d]{2,}|[^\\d\\.\\' + SUPPORTED_OPERANDS.join('\\') + ']';
-  var validationRegex = new RegExp(regexStr, 'gi');
-
-  if (validationRegex.test(equationStr)) {
-    throw new Error('The equation contains invalid characters. Supported operands: ' + SUPPORTED_OPERANDS.join(', '));
-  }
-
-};
-
-/**
- * Sanitizes the equation string:
- *   - strip whitespace
- *   - pad decimal points with missing zeros
- *
- * @param  {String} equationStr The equation
- * @return {String}             A sanitized equation
- */
-function sanitizeEquation(equationStr) {
-
-  return equationStr
-    // Strip whitespace
-    .replace(/\s+/g, '')
-    // Pad decimal points with zeros
-    .replace(/(\D)\.(\d)/g, function (m, p1, p2) {
-      return p1 + '0.' + p2;
-    })
-    .replace(/(\d)\.(\D)/g, function (m, p1, p2) {
-      return p1 + '.0' + p2;
-    });
-
-}
-
-/**
- * Checks if a given string contains operand characters
- *
- * @param  {String}  equationStr A string representation of an equation
- * @return {Boolean}             True if operand characters exist, otherwise false
- */
-function hasOperands (equationStr) {
-  SUPPORTED_OPERANDS_REGEX.lastIndex = 0;
-  return SUPPORTED_OPERANDS_REGEX.test(equationStr);
 }
 
 /**
