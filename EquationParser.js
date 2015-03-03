2d09fd7d04c702b722d49e40bf930074daec8f69
Marko <marko.raatikka@gmail.com>
2015-03-03 16:42:04 +0200

    EquationParser: clarify documentation

--- EquationParser.js
+++ EquationParser.js
@@ -1,7 +1,8 @@
 'use strict';
 
 /**
- * Parses a string-based equation into a tree of operands & numbers/Equation instances.
+ * Parses a string-based equation into a tree of operands &
+ * an array of numbers and/or Equation instances.
  *
  * For example, the equation 5*3-2+1-3 becomes:
  *
@@ -18,6 +19,9 @@
  *         *
  *        / \
  *       5   3
+ * 
+ * where an operand node & its immediate children (sub-equations) form an Equation.
+ * 
  */
 
 var Equation = require('./Equation');
@@ -27,11 +31,13 @@ var SUPPORTED_OPERANDS_REGEX = new RegExp('[\\' + SUPPORTED_OPERANDS.join('\\') 
 function EquationParser() {}
 
 /**
- * Validates a given equation string by checking that it contains
- * only numbers (integers or zero-padded floats) and/or supported operands
+ * Validates a given equation string by checking if it contains
+ *   - characters that are not numbers, decimal points or operands
+ *   - two or more consecutive non-numerical characters (e.g. ++, *., /-)
  *
  * @param  {String} equationStr
  * @throws {Error}  Throws an Error if the input is invalid
+ * @todo Horribly slow. We can do better (simplify)
  */
 function validateEquation(equationStr) {
 
@@ -62,18 +68,14 @@ function sanitizeEquation(equationStr) {
     .replace(/\s+/g, '')
     // Strip repeating operands and decimal points (e.g. 1+++2 => 1+2)
     .replace(repeatingCharsRegex, '')
-    // Pad decimal points with zeros
-    .replace(/(\D)\.(\d)/g, function (m, p1, p2) {
-      return p1 + '0.' + p2;
-    })
-    .replace(/(\d)\.(\D)/g, function (m, p1, p2) {
-      return p1 + '.0' + p2;
-    });
+    // Pad decimal points with zeros (leading & trailing)
+    .replace(/(\D)\.(\d)/g, function (m, p1, p2) { return p1 + '0.' + p2; })
+    .replace(/(\d)\.(\D)/g, function (m, p1, p2) { return p1 + '.0' + p2; });
 
 }
 
 /**
- * Checks if a given string contains operand characters
+ * Checks if a given string contains any of the supported operands
  *
  * @param  {String}  equationStr A string representation of an equation
  * @return {Boolean}             True if operand characters exist, otherwise false
@@ -85,7 +87,7 @@ function hasOperands (equationStr) {
 
 /**
  * Parses the string representation of an equation into an Equation instance
- * consisting of numbers and/or Equation instances
+ * consisting of numbers and/or other Equation instances.
  *
  * @param  {String} equationStr A string representation of an equation
  * @param  {Array} operands     An array of operands to process (defaults to SUPPORTED_OPERANDS)
@@ -99,7 +101,7 @@ function parseEquation(equationStr, operands) {
 
   operands = operands ? operands.slice() : SUPPORTED_OPERANDS.slice();
 
-  // Try splitting the equation into sub-equations by iterating over the operands
+  // Try splitting the equation into sub-equations using each of the supported operands
   while ((operand = operands.shift()) !== undefined) {
     subEquationStrings = equationStr.split(operand);
     if (subEquationStrings.length > 1) { break; }
@@ -138,6 +140,7 @@ EquationParser.prototype.parseEquation = function (equationStr) {
   // Validate
   validateEquation(equationStr)
 
+  // Parse
   return parseEquation.call(this, equationStr);
 
 };
